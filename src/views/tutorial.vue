8301955b7b3a8af7dc607497dbb6e27b638d4f76
Kiddle <a1044735733@163.com>
2024-12-21 12:26:47 +0800

    feat: tutorial

--- src/views/tutorial.vue
+++ src/views/tutorial.vue
@@ -8,7 +8,6 @@
     >
       <el-collapse v-model="activeNames">
         <el-collapse-item name="1">
-          <!-- 展示面板1 title -->
           <template #title>
             <i class="ti-layers"></i>&nbsp; A demo of scATAC-seq data analysis <sup><span
                 data-html="true"
@@ -21,15 +20,46 @@
                 > </i></span></sup>
           </template>
           <div class="panel-body">
-            <!-- 第一个面板 body 文字 -->
             <p>
-              In this tutorial, we use scATAC-seq data from a mouse spleen cell line with Prmt5 gene knockout to demonstrate the workflow of processing the gene activity matrix data obtained from the download page of the GP-scATACdb website to obtain a GRN. </p>
+              In this tutorial, we use scATAC-seq data to demonstrate the workflow of processing the gene activity matrix data to obtain a GRN. </p>
 
           </div>
 
         </el-collapse-item>
       </el-collapse>
     </div>
+    <!-- 展示面板2 -->
+    <div
+      class="col-md-12"
+      data-plugin-portlet
+      style="margin-top: 2%;margin-bottom: 3%;"
+    >
+      <el-collapse v-model="activeNames">
+        <el-collapse-item name="2">
+          <!-- 展示面板2 title -->
+          <template #title>
+            <i class="ti-layers"></i>&nbsp; Construct Gene Activity Matrix<sup><span
+                data-html="true"
+                data-toggle="tooltip"
+                data-placement="right"
+                title="<div align='left'> The meaning of columns is explained in the HELP page.</div>"
+              ><i
+                  class="ti-info-alt"
+                  style="font-size: 70%"
+                > </i></span></sup>
+          </template>
+          <div class="panel-body frame">
+            <embed
+              id="demo_html"
+              type="text/html"
+              src="1_GAM.html"
+              style="width: 100%;height: 950px; border:none"
+            >
+          </div>
+        </el-collapse-item>
+      </el-collapse>
+    </div>
+
     <!-- 展示面板2 -->
     <div
       class="col-md-12"
@@ -54,7 +84,7 @@
             <embed
               id="demo_html"
               type="text/html"
-              src="mm10_scATAC_GRN.html"
+              src="2_GRN.html"
               style="width: 100%;height: 950px; border:none"
             >
           </div>
@@ -85,7 +115,7 @@
             <embed
               id="demo_html"
               type="text/html"
-              src="filter_mat.html"
+              src="3_wt&ko.html"
               style="width: 100%;height: 950px; border:none"
             >
           </div>
@@ -116,7 +146,7 @@
             <embed
               id="demo_html"
               type="text/html"
-              src="refine_GRN.html"
+              src="4_refine.html"
               style="width: 100%;height: 950px; border:none"
             >
           </div>
